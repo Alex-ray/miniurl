c8815b244f7372b9f627930ac47b993c4e682edb
Apprentice <apprentice@devbootcamp.com>
2013-04-04 00:40:59 -0700

    styled login page

---
+++ db/migrate/20130403220847_urls.rb
@@ -0,0 +1,11 @@
+class Urls < ActiveRecord::Migration
+  def change
+    create_table :urls do |u|
+      u.string :long_url
+      u.string :short_url
+      u.integer :click_counter, :default => 0
+      u.integer :user_id
+      u.timestamps
+    end
+  end
+end
