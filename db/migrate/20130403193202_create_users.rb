fbc96aea80968b9ec7d796fa13aef00813f473eb
AlexRay <alex.james.ray@gmail.com>
2013-04-04 10:19:15 -0700

    added styles

--- db/migrate/20130403193202_create_users.rb
+++ db/migrate/20130403193202_create_users.rb
@@ -10,6 +10,17 @@ class CreateUsers < ActiveRecord::Migration
   end
 end
 
+# class CreateUrls < ActiveRecord::Migration
+#   def change
+#     create_table :urls do |u|
+#       u.string :long_url
+#       u.string :short_url
+#       u.integer :click_counter, :default => 0
+#       u.integer :user_id
+#       u.timestamps
+#     end
+#   end
+# end
 # class Urls < ActiveRecord::Migration
 #   def change
 #     create_table :urls do |u|
